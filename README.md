bf8e7415e436467937c5f089d2e14a6bd3cdd9a5
serg-kas <serg_kas@mail.ru>
2022-05-18 16:10:16 +0300

    background removing is ready

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # MaskR-CNN-demo
+Демонстрация работы предобученной сети Mask R-CNN.
+
+Ссылка на tensor_hub: https://hub.tensorflow.google.cn/tensorflow/mask_rcnn/inception_resnet_v2_1024x1024/1
+
+Функции программы:
+1. Object detection
+2. Instance segmentation
+3. Person detecting and remove background 
+
--- run.py
+++ run.py
@@ -131,27 +131,66 @@ def img_background(model, img_file, out_file):
                                            tf.uint8)
         result['detection_masks_reframed'] = detection_masks_reframed.numpy()
 
-    viz_utils.visualize_boxes_and_labels_on_image_array(
-        image_np_with_mask[0],
-        result['detection_boxes'][0],
-        (result['detection_classes'][0] + label_id_offset).astype(int),
-        result['detection_scores'][0],
-        category_index,
-        use_normalized_coordinates=True,
-        max_boxes_to_draw=200,
-        min_score_thresh=.30,
-        agnostic_mode=False,
-        instance_masks=result.get('detection_masks_reframed', None),
-        line_thickness=8,
-        # Чтобы не отрисовывать лишнее
-        skip_boxes=True,
-        skip_scores=True,
-        skip_labels=True)
+    # Оставим только класс person == 1
+    boxes = result['detection_boxes'][0]
+    classes = (result['detection_classes'][0] + label_id_offset).astype(int)
+    scores = result['detection_scores'][0]
+    #
+    indices = np.argwhere(classes == 1)
+    # boxes = np.squeeze(boxes[indices])
+    classes = np.squeeze(classes[indices])
+    scores = np.squeeze(scores[indices])
+    #
+    masks = result.get('detection_masks_reframed', None)
+    masks = np.squeeze(masks[indices])
 
-    plt.figure(figsize=(24, 32))
-    plt.imshow(image_np_with_mask[0])
-    # plt.show()
-    plt.savefig(out_file)
+    # Функция наложения маски
+    def apply_mask(image, mask):
+        image[:, :, 0] = np.where(
+            mask == 0,
+            125,
+            image[:, :, 0]
+        )
+        image[:, :, 1] = np.where(
+            mask == 0,
+            15,
+            image[:, :, 1]
+        )
+        image[:, :, 2] = np.where(
+            mask == 0,
+            15,
+            image[:, :, 2]
+        )
+        return image
+    #
+    # def apply_inst(image_orig, image_out, mask):
+    #     image_out[:, :, 0] = np.where(
+    #         mask != 0,
+    #         image_orig[:, :, 0],
+    #         125
+    #     )
+    #     image_out[:, :, 1] = np.where(
+    #         mask != 0,
+    #         image_orig[:, :, 1],
+    #         12
+    #     )
+    #     image_out[:, :, 2] = np.where(
+    #         mask != 0,
+    #         image_orig[:, :, 2],
+    #         15
+    #     )
+    #     return image_out
+
+    for i in range(classes.shape[0]):
+        if scores[i] > 0.9
+            image_bgrm = image_np[0].copy()
+            image_bgrm = apply_mask(image_bgrm, masks[i])
+            # TODO: Исправить формирование имён выходных файлов
+            curr_file = out_file[:-5] + '_' + str(i) + out_file[-5:]
+            print(curr_file, scores[i])
+            plt.figure(figsize=(24, 32))
+            plt.imshow(image_bgrm)
+            plt.savefig(curr_file)
 
     # имя и путь выходного файла сейчас не меняются
     return out_file
